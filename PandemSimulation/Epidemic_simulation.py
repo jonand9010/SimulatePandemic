e67d76a527b099c47e2695401c0431aa423ea6ca
jonand9010 <jonas.andersson@live.com>
2021-07-01 09:26:16 +0200

    Added plotting module

--- PandemSimulation/Epidemic_simulation.py
+++ PandemSimulation/Epidemic_simulation.py
@@ -19,9 +19,8 @@ import os
 
 from Network.Networks import SIRNetwork
 from Simulation.NetworkSimulation import SIR_NetworkSimulation
+from plotting.PandemPlots import WorldMapSimulation, SIRHistoryPlot
 
-
-#%%
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 seed = 0
@@ -35,44 +34,6 @@ SIR_simulation = SIR_NetworkSimulation(SIR_network, timesteps = 120)
 
 SIR_simulation.simulate(SIR_network)
 
-#%%
-fig1 = plt.figure()
-import cartopy.crs as ccrs
-ax1 = plt.subplot(111, projection = ccrs.PlateCarree())
-sns.set()
-#plt.close('all')
+WorldMapSimulation(SIR_network, SIR_simulation).run_simulation()
 
-for t in range(SIR_simulation.timesteps):
-
-    ax1.stock_img()
-    ax1.coastlines()
-    nx.draw_networkx(SIR_network.Graph, ax = ax1, font_size=10,
-             alpha=.25,
-             width=.1,
-             node_size=0.1*SIR_simulation.I[:,t],
-             with_labels=False,
-             pos=SIR_network.pos,
-             node_color = 'r',
-             cmap=plt.cm.autumn)
-
-    if t != SIR_simulation.timesteps-1:
-        if SIR_simulation.SIR[1, t] == 0:
-            break
-        
-        plt.pause(0.1)
-        ax1.cla()
-#%%
-
-fig2 = plt.figure()
-ax2 = plt.subplot(111)
-l1, = ax2.plot(SIR_simulation.SIR[0]/SIR_simulation.SIR[0,0], 'b', alpha = 0.5)
-l2, = ax2.plot(SIR_simulation.SIR[1]/SIR_simulation.SIR[0,0], 'r', alpha = 0.5)
-l3, = ax2.plot(SIR_simulation.SIR[2]/SIR_simulation.SIR[0,0], 'g', alpha = 0.5)
-ax2.legend([l1, l2, l3], ['Susceptible', 'Infected', 'Recovered'])
-ax2.set_title('R0: ' + str(SIR_network.R0))
-ax2.set_xlabel('Days')
-plt.show()
-    #plt.savefig('Epidemic' + str(t) + '.png')
-
- 
-# %%
+SIRHistoryPlot(SIR_simulation)
---
+++ PandemSimulation/plotting/PandemPlots.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import seaborn as sns
+import cartopy.crs as ccrs
+
+class WorldMapSimulation:
+
+    def __init__(self, Network, Simulation):
+        self.fig = plt.figure()
+        self.ax = plt.subplot(111, projection = ccrs.PlateCarree())
+        self.timesteps = Simulation.timesteps
+        self.Network = Network
+        self.Simulation = Simulation
+
+    def run_simulation(self):
+        for t in range(self.timesteps):
+
+            self.ax.stock_img()
+            self.ax.coastlines()
+            nx.draw_networkx(self.Network.Graph, ax = self.ax, font_size=10,
+                    alpha=.25,
+                    width=.1,
+                    node_size=0.1*self.Simulation.I[:,t],
+                    with_labels=False,
+                    pos=self.Network.pos,
+                    node_color = 'r',
+                    cmap=plt.cm.autumn)
+
+            if t != self.timesteps-1:
+                if self.Simulation.SIR[1, t] == 0:
+                    break
+                
+                plt.pause(0.1)
+                self.ax.cla()
+
+
+class SIRHistoryPlot():
+
+    def __init__(self, Simulation):
+        
+        self.fig = plt.figure()
+        self.ax = plt.subplot(111)
+        self.l1, = self.ax.plot(Simulation.SIR[0]/Simulation.SIR[0,0], 'b', alpha = 0.5)
+        self.l2, = self.ax.plot(Simulation.SIR[1]/Simulation.SIR[0,0], 'r', alpha = 0.5)
+        self.l3, = self.ax.plot(Simulation.SIR[2]/Simulation.SIR[0,0], 'g', alpha = 0.5)
+        self.ax.legend([self.l1, self.l2, self.l3], ['Susceptible', 'Infected', 'Recovered'])
+        self.ax.set_xlabel('Days')
+        plt.show()
+    
+    def save_fig(path = './'):
+        plt.savefig(path + 'Epidemic' + str(t) + '.png')
